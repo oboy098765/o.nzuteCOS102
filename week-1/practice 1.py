eff4ad4b3dc5866b021d80cf07679efbd9048308
oboy_098765 <onyedikachinzute@gmail.com>
2025-03-19 09:17:50 +0100

    ('Final')

--- week-1/practice 1.py
+++ week-1/practice 1.py
@@ -1,2 +1,15 @@
+# Integer number
+num = 100
+print(num)
+print("Data Type of variable is", type(num))
 
-print("Hello World!")
+# float number
+fnum = 34.45
+print(fnum)
+print("Data type of variable fnum is", type(fnum))
+
+# complex number
+cnum = 3 + 4j
+print(cnum)
+print("Data Type of variable cnum is", type(cnum))
+
